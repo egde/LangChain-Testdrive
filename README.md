08c4947d2405efd1189ca7234c3d99f3150c5ac3
egde <degertsa@yahoo.com>
2024-04-14 22:30:34 +0200

    Added Custom Tool to an agent

--- README.md
+++ README.md
@@ -4,23 +4,26 @@ In this repository I am working through LangChains by going through the samples 
 
 The code in the book is not up to date with the current changes at LangChain and OpenAI. So the code in the repp was working for me in April 2024 on a MacBook Pro M1 and Python 3.11
 
-## Tutorial1.py: OpenAI - Hello World
+## [Tutorial1.py](tutorial1.py): OpenAI - Hello World
 A simple LLM Setup with OpenAI
 
-## Tutorial2.py: OpenAI - Use Prompting
+## [Tutorial2.py](tutorial2.py): OpenAI - Use Prompting
 Use a prompt successfully to preprocess a text sucessfully
 
-## Tutorial3.py: Local LLM - Load an LLM from HuggingFace
+## [Tutorial3.py](tutorial3.py): Local LLM - Load an LLM from HuggingFace
 Use a local LLM for text generation
 
-## Tutorial4.py: Local LLM2 - Run a larger LLM from HuggingFace
+## [Tutorial4.py](tutorial4.py): Local LLM2 - Run a larger LLM from HuggingFace
 Use a larger LLM locally for text generation
 
-## Tutorial5.py: OpenAI - Build a Custom Chain using LCEL
+## [Tutorial5.py](tutorial5.py): OpenAI - Build a Custom Chain using LCEL
 Build a custom chain using LCEL.
 
-## Tutorial6.py: OpenAI - Building an Agent
+## [Tutorial6.py](tutorial6.py): OpenAI - Building an Agent
 Building an agent that uses Wikipedia and Youtube Tools
 
-### References
+## [Tutorial7.py](tutorial7.py): OpenAI - Build an Agent with a Custom Tool
+Integrate a custom tool with the agent
+
+# References
 Gupta, Mehul. LangChain in your Pocket: Beginner's Guide to Building Generative AI Applications using LLMs. Kindle Edition. 
---
+++ tutorial7.py
@@ -0,0 +1,78 @@
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain_openai import OpenAI
+from langchain_openai import OpenAI
+from langchain import hub
+from langchain.tools import BaseTool
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from langchain_community.tools.youtube.search import YouTubeSearchTool
+
+from pyowm.owm import OWM
+from pyowm.utils.config import get_default_config
+
+from typing import Type
+from pydantic import BaseModel, Field
+from decouple import config
+from loguru import logger
+
+# Defining the tools
+def get_weather(city):
+    config_dict = get_default_config()
+    config_dict['language'] = 'en'
+    owm = OWM(api_key=config('OPENWEATHER_API_KEY'))
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place(city)
+    
+    temperature = str(observation.weather.temperature("celsius")["temp"])+'°C'
+    humidity = str(observation.weather.humidity)+'%'
+    wind = str(observation.weather.wind)+'m/s'
+    status = observation.weather.detailed_status
+
+    return {
+        'temperature': temperature,
+        'humid': humidity,
+        'wind': wind,
+        'status': status
+    }
+
+class GetWeatherInput(BaseModel):
+    """Inputs for get_weather"""
+    city:str=Field(description='City name and country seperated by a comma')
+
+class GetWeatherTool(BaseTool):
+    name="get_weather"
+    description="""
+        Useful to get the weather details of a city.
+        Mandatory input format is 'city,country'
+        """
+    args_schema = GetWeatherInput
+
+    def _run(self, city:str):
+        weather = get_weather(city=city)
+        return weather
+
+
+tools = [GetWeatherTool()]
+
+prompt = hub.pull("hwchase17/react")
+
+memory = ConversationBufferMemory(memory_key="chat_history")
+llm = OpenAI(api_key=config("OPENAI_API_KEY"))
+
+agent = create_react_agent(llm=llm, prompt=prompt, tools=tools)
+
+agent_chain = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True, memory=memory
+)
+
+result = agent_chain.invoke(
+        input={
+            "input": "What can I wear for tomorrow in Frankfurt am Main?"
+        }
+    )
+
+logger.info(
+    "Result: \n"
+    + str(result.get('output'))
+)
